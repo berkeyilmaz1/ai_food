c3f584b9aa49e6ac319be4601fe3292befee93d4
berke.yz <berkeyilmaz189@gmail.com>
2025-01-31 16:37:53 +0300

    Update food service test to include path parameter in getFoods method

--- test/food_service_test.dart
+++ test/food_service_test.dart
@@ -3,12 +3,12 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   late final FoodService foodService;
-
+  const path = '';
   setUp(() {
     foodService = FoodService();
   });
   test('Food Service Test', () async {
-    final response = await foodService.getFoods();
+    final response = await foodService.getFoods(path);
     expect(response, isNotNull);
   });
 }
